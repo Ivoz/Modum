b767b7262a10840316d18b0d02a130ccc346b9c9
Ivoz <teh.ivo@gmail.com>
2012-02-06 05:48:54 +1100

    Tabs -> Spaces, because someone doesn't like fruit.

--- lib/config.py
+++ lib/config.py
@@ -5,49 +5,49 @@ import json
 class Config(collections.MutableMapping):
 
 
-	def __init__(self, config_path):
-		#self._settings = {
-			#'servers' : {
-				#'Default' : {
-					#'host'    : 'irc.oftc.net',
-					#'port'    : 6667,
-					#'ssl'     : False,
-					#'timeout' : 10,
-					#'nick'    : 'Modum',
-					#'channels' : ['#bots']
-					#}
-				#}
-			#}
-		self.load(config_path)
-		self.config_path = config_path
+    def __init__(self, config_path):
+        #self._settings = {
+            #'servers' : {
+                #'Default' : {
+                    #'host'    : 'irc.oftc.net',
+                    #'port'    : 6667,
+                    #'ssl'     : False,
+                    #'timeout' : 10,
+                    #'nick'    : 'Modum',
+                    #'channels' : ['#bots']
+                    #}
+                #}
+            #}
+        self.load(config_path)
+        self.config_path = config_path
 
-	def load(self, config_path):
-		self._settings = json.load(open(config_path,'r'), 
-				encoding="ASCII")
+    def load(self, config_path):
+        self._settings = json.load(open(config_path,'r'), 
+                encoding="ASCII")
 
-	def reload(self):
-		self.load(self.config_path)
+    def reload(self):
+        self.load(self.config_path)
 
-	def save(self, config_path = None):
-		if not config_path:
-			config_path = self.config_path
-		json.dump(self._settings, open(config_path,'w'), 
-				sort_keys = True, indent = 4)
+    def save(self, config_path = None):
+        if not config_path:
+            config_path = self.config_path
+        json.dump(self._settings, open(config_path,'w'), 
+                sort_keys = True, indent = 4)
 
-	def __getitem__(self, key):
-		return self._settings.__getitem__(key)
+    def __getitem__(self, key):
+        return self._settings.__getitem__(key)
 
-	def __setitem__(self, key, value):
-		self._settings.__setitem(key, value)
+    def __setitem__(self, key, value):
+        self._settings.__setitem(key, value)
 
-	def __delitem__(self, key):
-		self._settings.__delitem__(key)
+    def __delitem__(self, key):
+        self._settings.__delitem__(key)
 
-	def __iter__(self):
-		return self._settings.__iter__()
-	
-	def __len__(self):
-		return self._settings.__len__()
+    def __iter__(self):
+        return self._settings.__iter__()
+    
+    def __len__(self):
+        return self._settings.__len__()
 
-	def __getattr__(self, name):
-		return self._settings[name]
+    def __getattr__(self, name):
+        return self._settings[name]
--- modum.py
+++ modum.py
@@ -4,28 +4,32 @@ from lib.irc import Irc
 from lib.config import Config
 
 class Modum(object):
+    """ Modum, the Super Duper IRC bot """
 
-	def __init__(self, config_path):
-		self.root_path = os.path.abspath('') + '/'
-		self.config_path = config_path
-		self.conf = Config(self.root_path + config_path)
-		self.ircs = {}
-		for name in self.conf.servers.keys():
-			irc =  Irc(self.conf.servers[name], name)
-			self.ircs[name] = irc 
-	
-	def run(self):
-		for irc in self.ircs.values():
-			irc.connect()
-		self.bot = gevent.spawn(self._loop)
-		gevent.joinall([self.bot])
-	
-	def _loop(self):
-		while True:
-			for irc in self.ircs.values():
-				print irc.receive()
+    def __init__(self, config_path):
+#TODO: Change '/' to OS dependant directory-separator
+        self.root_path = os.path.abspath('') + '/'
+        self.config_path = config_path
+        self.conf = Config(self.root_path + config_path)
+        self.ircs = {}
+        for name in self.conf.servers.keys():
+            irc =  Irc(self.conf.servers[name], name)
+            self.ircs[name] = irc 
+    
+    """ Main method to start the bot up """
+    def run(self):
+        for irc in self.ircs.values():
+            irc.connect()
+        self.bot = gevent.spawn(self._loop)
+        self.bot.join()
+    
+    """ Main event loop """
+    def _loop(self):
+        while True:
+            for irc in self.ircs.values():
+                print irc.receive()
 
 
 if __name__ == "__main__":
-	ze_bot = Modum('config.json')
-	ze_bot.run()
+    ze_bot = Modum('config.json')
+    ze_bot.run()
