44298c56c2b3c91117e8ccc13d4d8f089ef49918
Ivoz <teh.ivo@gmail.com>
2012-11-16 14:20:03 +1100

    Updated setup

--- setup.py
+++ setup.py
@@ -1,21 +1,19 @@
-'''
-Modum
-=====
-
-Modum is a modular, multi-server, asynchronous IRC bot written in python.
-'''
+'''Modum is a modular, multi-server, asynchronous IRC bot written in python.'''
 
 from setuptools import setup, find_packages
+from lib.bot import __version__
+
+readme = open('README.rst').read()
 
 setup(
     name='Modum',
-    version='1.0dev',
+    version=__version__,
     url='https://github.com/Ivoz/Modum',
-    license='CC by-sa',
+    license='CC 3.0 by-sa',
     author='Matthew Iversen',
     author_email='teh.ivo@gmail.com',
-    description='Modum is a modular, multi-server, asynchronous IRC bot written in python.',
-    long_description=__doc__,
+    description=__doc__,
+    long_description=readme,
     packages=find_packages(),
-    zip_safe=False
+    zip_safe=False,
 )
