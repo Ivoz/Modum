916f42759363f3e16c931da7c9ca6dcfed08e5dd
Ivoz <teh.ivo@gmail.com>
2012-11-19 03:39:25 +1100

    Updated setup information

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
-'''Modum is a modular, multi-server, asynchronous IRC bot written in python.'''
+'''Modum is a modular, multi-server, asynchronous (gevent-based) IRC bot written in python.'''
 
 from setuptools import setup, find_packages
 from lib.bot import __version__
 
 readme = open('README.rst').read()
+package_name = 'Modum'
 
 setup(
-    name='Modum',
+    name=package_name,
     version=__version__,
     url='https://github.com/Ivoz/Modum',
     license='CC 3.0 by-sa',
@@ -14,6 +15,8 @@ setup(
     author_email='teh.ivo@gmail.com',
     description=__doc__,
     long_description=readme,
+    keywords='IRC bot gevent',
     packages=find_packages(),
-    zip_safe=False,
+    install_requires=['gevent'],
+    zip_safe=False
 )
