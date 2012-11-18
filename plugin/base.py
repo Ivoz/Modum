f88dab2633c657da7393b1be929b9351f903205a
Ivoz <teh.ivo@gmail.com>
2012-11-19 03:44:47 +1100

    Added ctcp ping, able to respond to nicknameinuse, etc

--- plugin/base.py
+++ plugin/base.py
@@ -10,6 +10,7 @@ class Base(Plugin):
         nick = self.client.nick
         self.nick(self.client.nick)
         self._send(Msg('USER', [nick, '8', '*', nick]))
+        self.connecting = True
 
     def on_ping(self, msg):
         msg.cmd = 'PONG'
@@ -19,12 +20,22 @@ class Base(Plugin):
         if self.client.nick == msg.nick:
             self.client.nick = msg.params[0]
 
+    def on_err_nicknameinuse(self, msg):
+        if self.connecting:
+            self.client.nick += '_'
+            self.nick(self.client.nick)
+
     def on_rpl_welcome(self, msg):
         channels = self.client.options['channels']
         self.join(channels)
+        self.connecting = False
+
+    def on_ctcp_ping(self, msg):
+        self.ctcp_reply('PING', msg.nick, msg.ctcp[1])
 
     def on_ctcp_version(self, msg):
-        self.ctcp_reply('VERSION', msg.nick, 'Modum v0.1')
+        from lib.bot import __version__ as version
+        self.ctcp_reply('VERSION', msg.nick, version)
 
     @command
     def help(self, msg):
@@ -35,6 +46,10 @@ class Base(Plugin):
         cmd_list = [p.commands for p in self.client.plugins.values()]
         names = {}
         [names.update(cmds) for cmds in cmd_list]
+        if 'Owner' in self.client.plugins:
+            if not self.client.plugins['Owner'].validate(msg):
+                [names.pop(k) for k, v in names.items()
+                        if hasattr(v, '_owner')]
         if msg.params[-1] in names:
             doc = names[msg.params[-1]].__doc__
             if doc is None:
